022315f092a59171831aad8831e1c5c3e647f997
hendrash <hendrash@mail.gvsu.edu>
2021-05-11 16:19:19 -0400

    subnet added

--- lib/dapp-cloud-formation-stack.ts
+++ lib/dapp-cloud-formation-stack.ts
@@ -1,9 +1,26 @@
 import * as cdk from '@aws-cdk/core';
-
+import {SubnetType, Vpc} from "@aws-cdk/aws-ec2"
 export class DappCloudFormationStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
     // The code that defines your stack goes here
+    let vpc= new Vpc(this,"master-vpn",{
+      cidr:"10.1.0.0/16",
+      subnetConfiguration:[{
+        cidrMask:24,
+        name: "Web network",
+        subnetType:SubnetType.PUBLIC
+      },
+    {
+      cidrMask:24,
+      name: "Application network",
+      subnetType: SubnetType.PRIVATE
+    },{
+      cidrMask:24,
+      name: "Data network",
+      subnetType: SubnetType.ISOLATED
+    }]
+    })
+  
   }
 }
