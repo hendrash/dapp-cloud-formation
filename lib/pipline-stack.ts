9d191aca1c9887b2aeb08d0c52c73e02288432dd
hendrash <hendrash@mail.gvsu.edu>
2021-05-12 18:47:32 -0400

    try again

--- lib/pipline-stack.ts
+++ lib/pipline-stack.ts
@@ -9,13 +9,14 @@ export class PiplineStack extends Stack {
         super(scope, id, props)
         let sourceArtifact = new cp.Artifact();
         let cloudAssemblyArtifact = new cp.Artifact();
+        try{
         const pipeline = new CdkPipeline(this, 'Pipeline', {
             pipelineName: "dapp-cloud-formation-pipline",
             cloudAssemblyArtifact,
             sourceAction: new cpa.GitHubSourceAction({
                 actionName: 'GitHub',
                 output: sourceArtifact,
-                oauthToken: SecretValue.secretsManager('arn:aws:secretsmanager:us-east-2:727818404418:secret:github-token-2-KXuf8t'),
+                oauthToken: SecretValue.plainText("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"),
                 trigger: cpa.GitHubTrigger.POLL,
                 owner: "hendrash",
                 repo: "master"
@@ -26,6 +27,10 @@ export class PiplineStack extends Stack {
                 buildCommand: 'npm run build'
             })
         })
+    }catch(error){
+        console.log(error)
+        console.log(SecretValue.secretsManager('arn:aws:secretsmanager:us-east-2:727818404418:secret:github-token-2-KXuf8t'))
+    }
 
 
 
